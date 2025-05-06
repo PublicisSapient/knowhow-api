378666944286c7dc510894aa582bcab11377058d
Shubh Narayan <shubh.narayan@publicissapient.com>
2025-05-06 16:35:58 +0530

    DTS-46302:Mongock sonar smells fixed
    Change-log:ITR commitment enhc

--- src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1320/ItrCommitmentChangeUnit.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1320/ItrCommitmentChangeUnit.java
@@ -29,6 +29,7 @@ import io.mongock.api.annotations.RollbackExecution;
 @ChangeUnit(id = "r_itr_commit_enhc", order = "013202", author = "shunaray", systemVersion = "13.2.0")
 public class ItrCommitmentChangeUnit {
 
+	public static final String JIRA_LABELS_KPI_120 = "jiraLabelsKPI120";
 	private final MongoTemplate mongoTemplate;
 
 	public ItrCommitmentChangeUnit(MongoTemplate mongoTemplate) {
@@ -37,17 +38,17 @@ public class ItrCommitmentChangeUnit {
 
 	@Execution
 	public void execution() {
-		mongoTemplate.getCollection("field_mapping_structure").deleteOne(new Document("fieldName", "jiraLabelsKPI120"));
+		mongoTemplate.getCollection("field_mapping_structure").deleteOne(new Document("fieldName", JIRA_LABELS_KPI_120));
 
 	}
 
 	@RollbackExecution
 	public void rollback() {
 		Document existingDocument = mongoTemplate.getCollection("field_mapping_structure")
-				.find(new Document("fieldName", "jiraLabelsKPI120")).first();
+				.find(new Document("fieldName", JIRA_LABELS_KPI_120)).first();
 
 		if (existingDocument == null) {
-			Document document = new Document().append("fieldName", "jiraLabelsKPI120")
+			Document document = new Document().append("fieldName", JIRA_LABELS_KPI_120)
 					.append("fieldLabel", "Labels for Scope Change Identification").append("fieldType", "chips")
 					.append("section", "WorkFlow Status Mapping").append("tooltip",
 							new Document("definition", "Specify labels to detect and track scope changes effectively"));
