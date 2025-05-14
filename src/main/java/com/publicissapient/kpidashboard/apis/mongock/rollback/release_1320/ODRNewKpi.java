f202c4347920853da5405dd6ed4c5451eace82f0
aksshriv1 <akshat.shrivastav@publicissapient.com>
2025-05-14 15:44:47 +0530

    DTS-45929 Open Defect Rate KPI
    Change Log : Code Smell

--- src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1320/ODRNewKpi.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1320/ODRNewKpi.java
@@ -14,6 +14,22 @@ import java.util.List;
 @ChangeUnit(id = "r_open_defect_rate_kpi", order = "013204", author = "aksshriv1", systemVersion = "13.2.0")
 public class ODRNewKpi {
 
+    public static final String FIELD_NAME = "fieldName";
+    public static final String FIELD_LABEL = "fieldLabel";
+    public static final String FIELD_TYPE = "fieldType";
+    public static final String FIELD_CATEGORY = "fieldCategory";
+    public static final String FIELD_DISPLAY_ORDER = "fieldDisplayOrder";
+    public static final String SECTION_ORDER = "sectionOrder";
+    public static final String SECTION = "section";
+    public static final String TOOLTIP = "tooltip";
+    public static final String DEFINITION = "definition";
+    public static final String KPI_ID = "kpiId";
+    public static final String COLUMN_NAME = "columnName";
+    public static final String IS_SHOWN = "isShown";
+    public static final String IS_DEFAULT = "isDefault";
+    public static final String WORK_FLOW_STATUS_MAPPING = "WorkFlow Status Mapping";
+    public static final String KPI_191 = "kpi191";
+    public static final String ORDER = "order";
     private final MongoTemplate mongoTemplate;
 
     public ODRNewKpi(MongoTemplate mongoTemplate) {
@@ -24,44 +40,44 @@ public class ODRNewKpi {
     public void rollback() {
         // Insert documents in field_mapping_structure
         List<Document> fieldMappingDocs = Arrays.asList(
-                new Document("fieldName", "jiraDefectRemovalStatusKPI191")
-                        .append("fieldLabel", "Status to identify closed defects")
-                        .append("fieldType", "chips")
-                        .append("fieldCategory", "workflow")
-                        .append("fieldDisplayOrder", 8)
-                        .append("sectionOrder", 4)
-                        .append("section", "WorkFlow Status Mapping")
-                        .append("tooltip", new Document("definition", "All workflow statuses used to identify defects in closed state")),
+                new Document(FIELD_NAME, "jiraDefectRemovalStatusKPI191")
+                        .append(FIELD_LABEL, "Status to identify closed defects")
+                        .append(FIELD_TYPE, "chips")
+                        .append(FIELD_CATEGORY, "workflow")
+                        .append(FIELD_DISPLAY_ORDER, 8)
+                        .append(SECTION_ORDER, 4)
+                        .append(SECTION, WORK_FLOW_STATUS_MAPPING)
+                        .append(TOOLTIP, new Document(DEFINITION, "All workflow statuses used to identify defects in closed state")),
 
-                new Document("fieldName", "resolutionTypeForRejectionKPI191")
-                        .append("fieldLabel", "Resolution type to be excluded")
-                        .append("fieldType", "chips")
-                        .append("section", "WorkFlow Status Mapping")
-                        .append("fieldDisplayOrder", 6)
-                        .append("sectionOrder", 4)
-                        .append("tooltip", new Document("definition", "Resolution types for defects that can be excluded from Defect count by Priority calculation")),
+                new Document(FIELD_NAME, "resolutionTypeForRejectionKPI191")
+                        .append(FIELD_LABEL, "Resolution type to be excluded")
+                        .append(FIELD_TYPE, "chips")
+                        .append(SECTION, WORK_FLOW_STATUS_MAPPING)
+                        .append(FIELD_DISPLAY_ORDER, 6)
+                        .append(SECTION_ORDER, 4)
+                        .append(TOOLTIP, new Document(DEFINITION, "Resolution types for defects that can be excluded from Defect count by Priority calculation")),
 
-                new Document("fieldName", "jiraDefectRejectionStatusKPI191")
-                        .append("fieldLabel", "Status to identify rejected defects")
-                        .append("fieldType", "text")
-                        .append("fieldCategory", "workflow")
-                        .append("section", "WorkFlow Status Mapping")
-                        .append("fieldDisplayOrder", 2)
-                        .append("sectionOrder", 4)
-                        .append("tooltip", new Document("definition", "All workflow statuses used to reject defects.")),
+                new Document(FIELD_NAME, "jiraDefectRejectionStatusKPI191")
+                        .append(FIELD_LABEL, "Status to identify rejected defects")
+                        .append(FIELD_TYPE, "text")
+                        .append(FIELD_CATEGORY, "workflow")
+                        .append(SECTION, WORK_FLOW_STATUS_MAPPING)
+                        .append(FIELD_DISPLAY_ORDER, 2)
+                        .append(SECTION_ORDER, 4)
+                        .append(TOOLTIP, new Document(DEFINITION, "All workflow statuses used to reject defects.")),
 
-                new Document("fieldName", "thresholdValueKPI191")
-                        .append("fieldLabel", "Target KPI Value")
-                        .append("fieldType", "number")
-                        .append("section", "Project Level Threshold")
-                        .append("fieldDisplayOrder", 1)
-                        .append("sectionOrder", 6)
-                        .append("tooltip", new Document("definition", "Target KPI value denotes the bare minimum a project should maintain for a KPI."))
+                new Document(FIELD_NAME, "thresholdValueKPI191")
+                        .append(FIELD_LABEL, "Target KPI Value")
+                        .append(FIELD_TYPE, "number")
+                        .append(SECTION, "Project Level Threshold")
+                        .append(FIELD_DISPLAY_ORDER, 1)
+                        .append(SECTION_ORDER, 6)
+                        .append(TOOLTIP, new Document(DEFINITION, "Target KPI value denotes the bare minimum a project should maintain for a KPI."))
         );
         mongoTemplate.getCollection("field_mapping_structure").insertMany(fieldMappingDocs);
 
         // Insert document in kpi_master
-        Document kpiMasterDoc = new Document("kpiId", "kpi191")
+        Document kpiMasterDoc = new Document(KPI_ID, KPI_191)
                 .append("kpiName", "Open Defect Rate")
                 .append("maxValue", "100")
                 .append("kpiUnit", "%")
@@ -72,7 +88,7 @@ public class ODRNewKpi {
                 .append("thresholdValue", "90")
                 .append("kanban", false)
                 .append("chartType", "line")
-                .append("kpiInfo", new Document("definition", "Measure of percentage of defects not closed against the total count tagged to the iteration")
+                .append("kpiInfo", new Document(DEFINITION, "Measure of percentage of defects not closed against the total count tagged to the iteration")
                         .append("formula", Arrays.asList(
                                 new Document("lhs", "ODR for a sprint")
                                         .append("operator", "division")
@@ -95,7 +111,7 @@ public class ODRNewKpi {
         mongoTemplate.getCollection("kpi_master").insertOne(kpiMasterDoc);
 
         // Insert document in kpi_category_mapping
-        Document kpiCategoryMappingDoc = new Document("kpiId", "kpi191")
+        Document kpiCategoryMappingDoc = new Document(KPI_ID, KPI_191)
                 .append("categoryId", "categoryTwo")
                 .append("kpiOrder", 15)
                 .append("kanban", false);
@@ -103,18 +119,18 @@ public class ODRNewKpi {
 
         // Insert document in kpi_column_configs
         Document kpiColumnConfigDoc = new Document("basicProjectConfigId", null)
-                .append("kpiId", "kpi191")
+                .append(KPI_ID, KPI_191)
                 .append("kpiColumnDetails", Arrays.asList(
-                        new Document("columnName", "Sprint Name").append("order", 1).append("isShown", true).append("isDefault", true),
-                        new Document("columnName", "Defect ID").append("order", 2).append("isShown", true).append("isDefault", true),
-                        new Document("columnName", "Defect Description").append("order", 3).append("isShown", true).append("isDefault", true),
-                        new Document("columnName", "Story ID").append("order", 4).append("isShown", true).append("isDefault", true),
-                        new Document("columnName", "Story Description").append("order", 5).append("isShown", true).append("isDefault", true),
-                        new Document("columnName", "Squad").append("order", 6).append("isShown", true).append("isDefault", true),
-                        new Document("columnName", "Root Cause").append("order", 7).append("isShown", true).append("isDefault", true),
-                        new Document("columnName", "Defect Priority").append("order", 8).append("isShown", true).append("isDefault", true),
-                        new Document("columnName", "Defect Status").append("order", 10).append("isShown", true).append("isDefault", true),
-                        new Document("columnName", "Time Spent (in hours)").append("order", 11).append("isShown", true).append("isDefault", true)
+                        new Document(COLUMN_NAME, "Sprint Name").append(ORDER, 1).append(IS_SHOWN, true).append(IS_DEFAULT, true),
+                        new Document(COLUMN_NAME, "Defect ID").append(ORDER, 2).append(IS_SHOWN, true).append(IS_DEFAULT, true),
+                        new Document(COLUMN_NAME, "Defect Description").append(ORDER, 3).append(IS_SHOWN, true).append(IS_DEFAULT, true),
+                        new Document(COLUMN_NAME, "Story ID").append(ORDER, 4).append(IS_SHOWN, true).append(IS_DEFAULT, true),
+                        new Document(COLUMN_NAME, "Story Description").append(ORDER, 5).append(IS_SHOWN, true).append(IS_DEFAULT, true),
+                        new Document(COLUMN_NAME, "Squad").append(ORDER, 6).append(IS_SHOWN, true).append(IS_DEFAULT, true),
+                        new Document(COLUMN_NAME, "Root Cause").append(ORDER, 7).append(IS_SHOWN, true).append(IS_DEFAULT, true),
+                        new Document(COLUMN_NAME, "Defect Priority").append(ORDER, 8).append(IS_SHOWN, true).append(IS_DEFAULT, true),
+                        new Document(COLUMN_NAME, "Defect Status").append(ORDER, 10).append(IS_SHOWN, true).append(IS_DEFAULT, true),
+                        new Document(COLUMN_NAME, "Time Spent (in hours)").append(ORDER, 11).append(IS_SHOWN, true).append(IS_DEFAULT, true)
                 ));
         mongoTemplate.getCollection("kpi_column_configs").insertOne(kpiColumnConfigDoc);
     }
@@ -122,10 +138,10 @@ public class ODRNewKpi {
     @Execution
     public void execution() {
 
-        mongoTemplate.getCollection("field_mapping_structure").deleteMany(new Document("fieldName", new Document("$in", Arrays.asList(
+        mongoTemplate.getCollection("field_mapping_structure").deleteMany(new Document(FIELD_NAME, new Document("$in", Arrays.asList(
                 "jiraDefectRemovalStatusKPI191", "resolutionTypeForRejectionKPI191", "jiraDefectRejectionStatusKPI191", "thresholdValueKPI191"))));
-        mongoTemplate.getCollection("kpi_master").deleteOne(new Document("kpiId", "kpi191"));
-        mongoTemplate.getCollection("kpi_category_mapping").deleteOne(new Document("kpiId", "kpi191"));
-        mongoTemplate.getCollection("kpi_column_configs").deleteOne(new Document("kpiId", "kpi191"));
+        mongoTemplate.getCollection("kpi_master").deleteOne(new Document(KPI_ID, KPI_191));
+        mongoTemplate.getCollection("kpi_category_mapping").deleteOne(new Document(KPI_ID, KPI_191));
+        mongoTemplate.getCollection("kpi_column_configs").deleteOne(new Document(KPI_ID, KPI_191));
     }
 }
