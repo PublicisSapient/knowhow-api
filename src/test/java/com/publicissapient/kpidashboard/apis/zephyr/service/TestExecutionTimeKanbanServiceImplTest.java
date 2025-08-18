08f001421a951c717eb154d2e6ee0051d4d825e6
rendk <renukaprasad.dk@publicissapient.com>
2025-08-18 15:36:00 +0530

    DTS-48069, Test cases

---
+++ src/test/java/com/publicissapient/kpidashboard/apis/zephyr/service/TestExecutionTimeKanbanServiceImplTest.java
@@ -0,0 +1,191 @@
+/*******************************************************************************
+ * Copyright 2014 CapitalOne, LLC.
+ * Further development Copyright 2022 Sapient Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.publicissapient.kpidashboard.apis.zephyr.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.publicissapient.kpidashboard.apis.enums.KPICode;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.publicissapient.kpidashboard.apis.appsetting.service.ConfigHelperService;
+import com.publicissapient.kpidashboard.apis.common.service.CacheService;
+import com.publicissapient.kpidashboard.apis.common.service.CommonService;
+import com.publicissapient.kpidashboard.apis.common.service.impl.KpiHelperService;
+import com.publicissapient.kpidashboard.apis.config.CustomApiConfig;
+import com.publicissapient.kpidashboard.apis.constant.Constant;
+import com.publicissapient.kpidashboard.apis.data.AccountHierarchyFilterDataFactory;
+import com.publicissapient.kpidashboard.apis.data.KanbanJiraIssueDataFactory;
+import com.publicissapient.kpidashboard.apis.data.KpiRequestFactory;
+import com.publicissapient.kpidashboard.apis.data.TestCaseDetailsDataFactory;
+import com.publicissapient.kpidashboard.apis.enums.Filters;
+import com.publicissapient.kpidashboard.apis.enums.KPISource;
+import com.publicissapient.kpidashboard.apis.errors.ApplicationException;
+import com.publicissapient.kpidashboard.apis.model.AccountHierarchyData;
+import com.publicissapient.kpidashboard.apis.model.KpiElement;
+import com.publicissapient.kpidashboard.apis.model.KpiRequest;
+import com.publicissapient.kpidashboard.apis.model.Node;
+import com.publicissapient.kpidashboard.apis.model.TreeAggregatorDetail;
+import com.publicissapient.kpidashboard.apis.util.KPIHelperUtil;
+import com.publicissapient.kpidashboard.common.constant.ProcessorConstants;
+import com.publicissapient.kpidashboard.common.model.application.DataCount;
+import com.publicissapient.kpidashboard.common.model.application.FieldMapping;
+import com.publicissapient.kpidashboard.common.model.application.ProjectBasicConfig;
+import com.publicissapient.kpidashboard.common.model.application.ProjectToolConfig;
+import com.publicissapient.kpidashboard.common.model.jira.KanbanJiraIssue;
+import com.publicissapient.kpidashboard.common.model.zephyr.TestCaseDetails;
+import com.publicissapient.kpidashboard.common.repository.jira.KanbanJiraIssueRepository;
+import com.publicissapient.kpidashboard.common.repository.zephyr.TestCaseDetailsRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestExecutionTimeKanbanServiceImplTest {
+
+    private static final String TESTCASEKEY = "testCaseData";
+    public Map<String, ProjectBasicConfig> projectConfigMap = new HashMap<>();
+    public Map<ObjectId, FieldMapping> fieldMappingMap = new HashMap<>();
+    List<KanbanJiraIssue> totalTestCaseList = new ArrayList<>();
+    List<TestCaseDetails> testCaseDetailsList = new ArrayList<>();
+    @Mock
+    KanbanJiraIssueRepository kanbanFeatureRepository;
+    @Mock
+    CacheService cacheService;
+    @Mock
+    ConfigHelperService configHelperService;
+    @Mock
+    KpiHelperService kpiHelperService;
+    @InjectMocks
+    TestExecutionTimeKanbanServiceImpl testExecutionTimeKanbanServiceImpl;
+    @Mock
+    TestCaseDetailsRepository testCaseDetailsRepository;
+    @Mock
+    CustomApiConfig customApiConfig;
+    private List<FieldMapping> fieldMappingList = new ArrayList<>();
+    private KpiRequest kpiRequest;
+    private KpiElement kpiElement;
+    private Map<String, String> kpiWiseAggregation = new HashMap<>();
+    private List<AccountHierarchyData> accountHierarchyDataList = new ArrayList<>();
+    @Mock
+    private CommonService commonService;
+
+    private List<ProjectBasicConfig> projectConfigList = new ArrayList<>();
+
+    @Before
+    public void setup() {
+        KpiRequestFactory kpiRequestFactory = KpiRequestFactory.newInstance();
+        AccountHierarchyFilterDataFactory accountHierarchyFilterDataFactory = AccountHierarchyFilterDataFactory
+                .newInstance();
+
+        ProjectBasicConfig projectBasicConfig = new ProjectBasicConfig();
+        projectBasicConfig.setId(new ObjectId("6335363749794a18e8a4479b"));
+        projectBasicConfig.setIsKanban(true);
+        projectBasicConfig.setProjectName("Scrum Project");
+        projectBasicConfig.setProjectNodeId("Scrum Project_6335363749794a18e8a4479b");
+        projectConfigList.add(projectBasicConfig);
+
+        projectConfigList.forEach(projectConfig -> {
+            projectConfigMap.put(projectConfig.getProjectName(), projectConfig);
+        });
+        Mockito.when(cacheService.cacheProjectConfigMapData()).thenReturn(projectConfigMap);
+
+        accountHierarchyDataList = accountHierarchyFilterDataFactory.getAccountHierarchyDataList();
+        totalTestCaseList = KanbanJiraIssueDataFactory.newInstance().getKanbanJiraIssueDataList();
+        testCaseDetailsList = TestCaseDetailsDataFactory.newInstance().getTestCaseDetailsList();
+        kpiRequest = kpiRequestFactory.findKpiRequest("kpi63");
+        kpiRequest.setLabel("PROJECT");
+        kpiElement = kpiRequest.getKpiList().get(0);
+        kpiWiseAggregation.put("defectInjectionRate", "average");
+        Map<ObjectId, Map<String, List<ProjectToolConfig>>> toolMap = new HashMap<>();
+        Map<String, List<ProjectToolConfig>> projectTool = new HashMap<>();
+
+        ProjectToolConfig zephyConfig = new ProjectToolConfig();
+        zephyConfig.setRegressionAutomationLabels(Arrays.asList("test1"));
+        zephyConfig.setTestRegressionValue(Arrays.asList("test1"));
+        zephyConfig.setRegressionAutomationFolderPath(Arrays.asList("test1"));
+        projectTool.put(ProcessorConstants.ZEPHYR, Arrays.asList(zephyConfig));
+        ProjectToolConfig jiraTest = new ProjectToolConfig();
+        jiraTest.setJiraRegressionTestValue(Arrays.asList("test1"));
+        jiraTest.setTestCaseStatus(Arrays.asList("test1"));
+        projectTool.put(ProcessorConstants.ZEPHYR, Arrays.asList(zephyConfig));
+        projectTool.put(ProcessorConstants.JIRA_TEST, Arrays.asList(jiraTest));
+        toolMap.put(new ObjectId("6335363749794a18e8a4479b"), projectTool);
+        when(cacheService.cacheProjectToolConfigMapData()).thenReturn(toolMap);
+    }
+
+    @Test
+    public void testGetAutomatedTestPercentage() throws ApplicationException {
+        List<Node> leafNodeList = new ArrayList<>();
+        TreeAggregatorDetail treeAggregatorDetail = KPIHelperUtil.getTreeLeafNodesGroupedByFilter(kpiRequest,
+                accountHierarchyDataList, new ArrayList<>(), "hierarchyLevelOne", 5);
+        treeAggregatorDetail.getMapOfListOfLeafNodes().forEach((k, v) -> {
+            if (Filters.getFilter(k) == Filters.SPRINT) {
+                leafNodeList.addAll(v);
+            }
+        });
+        when(testCaseDetailsRepository.findTestDetails(any(), any(), any())).thenReturn(testCaseDetailsList);
+        when(configHelperService.getFieldMappingMap()).thenReturn(fieldMappingMap);
+        String kpiRequestTrackerId = "Excel-Zephyr-5be544de025de212549176a9";
+        when(cacheService.getFromApplicationCache(Constant.KPI_REQUEST_TRACKER_ID_KEY + KPISource.ZEPHYRKANBAN.name()))
+                .thenReturn(kpiRequestTrackerId);
+        try {
+            KpiElement kpiElement = testExecutionTimeKanbanServiceImpl.getKpiData(kpiRequest,
+                    kpiRequest.getKpiList().get(0), treeAggregatorDetail);
+            assertThat("Regression Percentage Value :", ((List<DataCount>) kpiElement.getTrendValueList()).size(),
+                    equalTo(1));
+        } catch (ApplicationException enfe) {
+
+        }
+    }
+
+    @Test
+    public void testGetQualifierType() {
+        assertThat("Kpi Name :", testExecutionTimeKanbanServiceImpl.getQualifierType(),
+                equalTo(KPICode.TEST_EXECUTION_TIME_KANBAN.name()));
+    }
+
+    @Test
+    public void fetchKPIDataFromDb() throws ApplicationException {
+        List<Node> leafNodeList = new ArrayList<>();
+        TreeAggregatorDetail treeAggregatorDetail = KPIHelperUtil.getTreeLeafNodesGroupedByFilter(kpiRequest,
+                accountHierarchyDataList, new ArrayList<>(), "hierarchyLevelOne", 5);
+        treeAggregatorDetail.getMapOfListOfLeafNodes().forEach((k, v) -> {
+            if (Filters.getFilter(k) == Filters.SPRINT) {
+                leafNodeList.addAll(v);
+            }
+        });
+        when(testCaseDetailsRepository.findTestDetails(any(), any(), any())).thenReturn(testCaseDetailsList);
+        Map<String, Object> defectDataListMap = testExecutionTimeKanbanServiceImpl.fetchKPIDataFromDb(leafNodeList, null,
+                null, kpiRequest);
+        assertThat("Total Test Case value :", (Arrays.asList(defectDataListMap.get(TESTCASEKEY)).size()), equalTo(1));
+    }
+}
