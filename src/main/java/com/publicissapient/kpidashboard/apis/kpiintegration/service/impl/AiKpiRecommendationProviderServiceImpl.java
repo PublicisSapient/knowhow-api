4108863bd5874e3c63a101a1535c12685ac5e176
rendk <renukaprasad.dk@publicissapient.com>
2025-09-03 11:06:29 +0530

    DTS-48809, For testing

--- src/main/java/com/publicissapient/kpidashboard/apis/kpiintegration/service/impl/AiKpiRecommendationProviderServiceImpl.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/kpiintegration/service/impl/AiKpiRecommendationProviderServiceImpl.java
@@ -171,10 +171,10 @@ public class AiKpiRecommendationProviderServiceImpl implements KpiRecommendation
 		JsonNode jsonArray = ((ObjectNode) responseObject).get("project_recommendations");
 		jsonArray.forEach(jsonElement -> {
 			GenericKpiRecommendation genericRecommendationItem = new GenericKpiRecommendation();
-			genericRecommendationItem.setObservation(String.valueOf(jsonElement.get("observation").asText()));
+
 			genericRecommendationItem.setRecommendationDetails(String.valueOf(jsonElement.get("recommendation").asText()));
-			genericRecommendationItem.setKpiName(String.valueOf(jsonElement.get("kpi").asText()));
-			genericRecommendationItem.setObservation(String.valueOf(jsonElement.get("observation").asText()));
+
+
 			genericRecommendationItem.setRecommendationType(String.valueOf(jsonElement.get("severity").asText()));
 			List<String> correlatedKpis = new ArrayList<>();
 			for (JsonNode correlatedKpi : jsonElement.withArray("correlated_kpis")) {
