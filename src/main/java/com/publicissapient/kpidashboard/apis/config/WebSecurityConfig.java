ad444bfe9bac8e8f4712c13d8d03ba3dd2e4999d
Shubh Narayan <shubh.narayan@publicissapient.com>
2025-09-17 11:29:38 +0530

    refactor: update method security configuration to use global method security

--- src/main/java/com/publicissapient/kpidashboard/apis/config/WebSecurityConfig.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -69,7 +69,7 @@ import lombok.AllArgsConstructor;
 @EnableWebSecurity
 @EnableConfigurationProperties
 @AllArgsConstructor
-@EnableMethodSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig implements WebMvcConfigurer {
 
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
