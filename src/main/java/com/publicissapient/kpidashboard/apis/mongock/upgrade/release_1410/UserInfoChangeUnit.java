a9150697c401a74c446557397bd8a2cadc9e0b65
Gurdeep Singh <gurdeep.singh@publicissapient.com>
2025-10-29 20:35:01 +0530

    added copyright for new files

--- src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1410/UserInfoChangeUnit.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1410/UserInfoChangeUnit.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2024 <Sapient Corporation>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the
+ *  License.
+ */
 package com.publicissapient.kpidashboard.apis.mongock.upgrade.release_1410;
 
 import com.mongodb.client.MongoCollection;
@@ -89,13 +104,6 @@ public class UserInfoChangeUnit {
 
     private void rollbackUserInfoCollection() {
         MongoCollection<Document> collection = mongoTemplate.getCollection("user_info");
-        /*Document unsetFields = new Document()
-                .append(FIELD_UPDATED_ON, "")
-                .append(FIELD_UPDATED_BY, "")
-                .append(FIELD_CREATED_BY, "");
-
-        collection.updateMany(new Document(), new Document("$unset", unsetFields));
-*/
         collection.find().forEach(doc -> {
             Document updateFields = new Document();
             Document unsetFields = new Document();
