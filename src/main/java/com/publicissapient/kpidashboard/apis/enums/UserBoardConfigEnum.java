735cdb88186049942e6cf22562507e4d7bc336f2
Gurdeep Singh <gurdeep.singh@publicissapient.com>
2025-11-07 11:58:28 +0530

    name changes

--- src/main/java/com/publicissapient/kpidashboard/apis/enums/UserBoardConfigEnum.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/enums/UserBoardConfigEnum.java
@@ -26,7 +26,7 @@ import lombok.Getter;
  */
 @Getter
 public enum UserBoardConfigEnum {
-	SCRUM_KANBAN_BOARD(Arrays.asList("Iteration", "Developer", "Dora", "Release", "Backlog","PEB")),
+	SCRUM_KANBAN_BOARD(Arrays.asList("Iteration", "Developer", "Dora", "Release", "Backlog","Potential Economic Benefits")),
 	OTHER_BOARD(Arrays.asList("KPI Maturity"));
 
 	private final List<String> boardName;
