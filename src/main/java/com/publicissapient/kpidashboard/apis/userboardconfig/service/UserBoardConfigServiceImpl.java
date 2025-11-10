034b6c785bd044681286c6d0e0d360c4a0feda20
Gurdeep Singh <gurdeep.singh@publicissapient.com>
2025-11-10 22:44:48 +0530

    moved hardcoded value to constant

--- src/main/java/com/publicissapient/kpidashboard/apis/userboardconfig/service/UserBoardConfigServiceImpl.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/userboardconfig/service/UserBoardConfigServiceImpl.java
@@ -366,7 +366,8 @@ public class UserBoardConfigServiceImpl implements UserBoardConfigService {
 				(boards, offset) ->
 						boards.forEach(
 								boardDTO -> {
-									if (boardDTO.getBoardId() == 0 || boardDTO.getBoardId() == 12 ) {
+									if (boardDTO.getBoardId() == 0
+											|| boardDTO.getBoardId() == Constant.DEFAULT_BOARD_ID_FOR_PEB) {
 										boardDTO.setFilters(copyFiltersWithoutId(filtersMap.get(0)));
 									} else {
 										boardDTO.setFilters(
