59cdb8f63adff6f58a2ba9498c21e7f722bf5697
Gurdeep Singh <gurdeep.singh@publicissapient.com>
2025-11-19 18:57:56 +0530

    change in imports.

--- src/test/java/com/publicissapient/kpidashboard/apis/forecast/ForecastingManagerTest.java
+++ src/test/java/com/publicissapient/kpidashboard/apis/forecast/ForecastingManagerTest.java
@@ -17,7 +17,10 @@
 
 package com.publicissapient.kpidashboard.apis.forecast;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;	
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
