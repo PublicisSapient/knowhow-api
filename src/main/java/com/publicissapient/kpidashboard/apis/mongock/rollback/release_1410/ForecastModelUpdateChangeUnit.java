52ba6c8599d6d1a04def0518857e497b02b9b81b
Gurdeep Singh <gurdeep.singh@publicissapient.com>
2025-11-19 13:15:25 +0530

    adding in kpi_master and removing the other one.

--- src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1410/ForecastModelUpdateChangeUnit.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1410/ForecastModelUpdateChangeUnit.java
@@ -69,7 +69,6 @@ public class ForecastModelUpdateChangeUnit {
 		// Adding data set for Iteration Board.
 		removeForecastModel(KPICode.ITERATION_BURNUP.getKpiId());
 		removeForecastModel(KPICode.LATE_REFINEMENT.getKpiId());
-		removeForecastModel(KPICode.NEXT_SPRINT_LATE_REFINEMENT.getKpiId());
 
 		log.info("Completed rollback of forecast model updates");
 	}
@@ -116,9 +115,6 @@ public class ForecastModelUpdateChangeUnit {
 				KPICode.ITERATION_BURNUP.getKpiId(), ForecastingModel.EXPONENTIAL_SMOOTHING.getName());
 		updateForecastModel(
 				KPICode.LATE_REFINEMENT.getKpiId(), ForecastingModel.EXPONENTIAL_SMOOTHING.getName());
-		updateForecastModel(
-				KPICode.NEXT_SPRINT_LATE_REFINEMENT.getKpiId(),
-				ForecastingModel.EXPONENTIAL_SMOOTHING.getName());
 
 		log.info("Completed forecast model update for 3 KPIs");
 	}
