a158352778bc236af90ee2d2ef62cf1742ea1286
aksshriv1 <akshat.shrivastav@publicissapient.com>
2025-11-26 15:15:50 +0530

    REST controller for managing help and documentation resources.

---
+++ src/main/java/com/publicissapient/kpidashboard/apis/appsetting/rest/PEBConfigController.java
@@ -0,0 +1,75 @@
+package com.publicissapient.kpidashboard.apis.appsetting.rest;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.publicissapient.kpidashboard.apis.appsetting.config.PEBConfig;
+import com.publicissapient.kpidashboard.apis.model.ServiceResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/peb")
+@Tag(name = "PotentialEconomicBenefit", description = "Endpoints for PEB Calculator Configuration")
+@Slf4j
+public class PEBConfigController {
+
+	@Autowired private PEBConfig pebConfig;
+
+	public void setPebConfig(PEBConfig pebConfig) {
+		this.pebConfig = pebConfig;
+	}
+
+	/**
+	 * Retrieves Potential Economic Benefits configuration.
+	 *
+	 * @return ResponseEntity containing PEB configuration parameters
+	 */
+	@GetMapping(value = "/configs", produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(
+			summary = "Get Economic Benefits Configuration",
+			description = "Retrieves Potential Economic Benefits calculator configuration parameters")
+	@ApiResponses(
+			value = {
+				@ApiResponse(
+						responseCode = "200",
+						description = "Successfully retrieved PEB configuration",
+						content = {
+							@Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = ServiceResponse.class))
+						})
+			})
+	public ResponseEntity<ServiceResponse> getEconomicBenefitsConfig() {
+		log.info("Fetching economic benefits configuration");
+		Map<String, Object> pebData = new LinkedHashMap<>();
+		pebData.put(
+				"totalDevelopers",
+				pebConfig.getTotalDevelopers() != null ? pebConfig.getTotalDevelopers() : 30);
+		pebData.put(
+				"avgCostPerDeveloper",
+				pebConfig.getAvgCostPerDeveloper() != null
+						? pebConfig.getAvgCostPerDeveloper()
+						: 100000.00);
+		pebData.put(
+				"timeDuration",
+				pebConfig.getTimeDuration() != null ? pebConfig.getTimeDuration() : "Per Year");
+		log.info("Economic benefits configuration retrieved successfully");
+		return ResponseEntity.ok(
+				new ServiceResponse(
+						true, "Economic benefits configuration retrieved successfully", pebData));
+	}
+}
