a3580d6c5fcf7ee3d499288c263b2cf6fa978212
kunkambl <kunal.kamble@publicissapient.com>
2025-11-27 09:56:53 +0530

    DTS-49747: Updated kpi correlation matrix to latest data

--- src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1410/UpdateKpiCorrelationMatrixPromptDetails.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/mongock/rollback/release_1410/UpdateKpiCorrelationMatrixPromptDetails.java
@@ -1,18 +1,35 @@
-package com.publicissapient.kpidashboard.apis.mongock.upgrade.release_1410;
+/*
+ *  Copyright 2024 <Sapient Corporation>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the
+ *  License.
+ */
 
-import io.mongock.api.annotations.ChangeUnit;
-import io.mongock.api.annotations.Execution;
-import io.mongock.api.annotations.RollbackExecution;
+package com.publicissapient.kpidashboard.apis.mongock.rollback.release_1410;
+
+import java.util.Arrays;
+
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
-import java.util.Arrays;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
 
 @ChangeUnit(
-        id = "update_kpi_correlation_matrix_for_recommendation_prompt",
-        order = "14109",
+        id = "r_update_kpi_correlation_matrix_for_recommendation_prompt",
+        order = "014110",
         author = "kunkambl",
         systemVersion = "14.1.0")
 public class UpdateKpiCorrelationMatrixPromptDetails {
@@ -25,7 +42,7 @@ public class UpdateKpiCorrelationMatrixPromptDetails {
         this.mongoTemplate = mongoTemplate;
     }
 
-    @Execution
+    @RollbackExecution
     public void execution() {
 
         mongoTemplate.updateFirst(
@@ -67,7 +84,7 @@ public class UpdateKpiCorrelationMatrixPromptDetails {
 
     }
 
-    @RollbackExecution
+    @Execution
     public void rollback() {
 
         mongoTemplate.updateFirst(
