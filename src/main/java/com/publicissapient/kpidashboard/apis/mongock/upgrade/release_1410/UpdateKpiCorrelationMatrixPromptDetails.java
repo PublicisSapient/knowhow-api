2fa0b6d12eede8a77467c4ab9b82d559a1b0af7a
kunkambl <kunal.kamble@publicissapient.com>
2025-11-27 09:54:22 +0530

    DTS-49747: Updated kpi correlation matrix to latest data

---
+++ src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1410/UpdateKpiCorrelationMatrixPromptDetails.java
@@ -0,0 +1,11 @@
+package com.publicissapient.kpidashboard.apis.mongock.upgrade.release_1410;
+
+import io.mongock.api.annotations.ChangeUnit;
+
+@ChangeUnit(
+        id = "update_kpi_correlation_matrix_for_recommendation_prompt",
+        order = "14109",
+        author = "kunkambl",
+        systemVersion = "14.1.0")
+public class UpdateKpiCorrelationMatrixPromtDetails {
+}
