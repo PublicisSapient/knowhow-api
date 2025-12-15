6da13ac5abbdc0617789750814446918647d3cc6
Gurdeep Singh <gurdeep.singh@publicissapient.com>
2025-12-15 10:24:50 +0530

    removing forecast model for Test Execution Time. Ticket : DTS-51131

--- src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1410/ForecastModelUpdateChangeUnit.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/mongock/upgrade/release_1410/ForecastModelUpdateChangeUnit.java
@@ -86,8 +86,6 @@ public class ForecastModelUpdateChangeUnit {
 				KPICode.TEST_EXECUTION_AND_PASS_PERCENTAGE.getKpiId(),
 				ForecastingModel.EXPONENTIAL_SMOOTHING.getName());
 		updateForecastModel(
-				KPICode.TEST_EXECUTION_TIME.getKpiId(), ForecastingModel.EXPONENTIAL_SMOOTHING.getName());
-		updateForecastModel(
 				KPICode.TEST_EXECUTION_KANBAN.getKpiId(), ForecastingModel.EXPONENTIAL_SMOOTHING.getName());
 
 		// KPIs for Iteration Board.
@@ -142,7 +140,6 @@ public class ForecastModelUpdateChangeUnit {
 		removeForecastModel(KPICode.CODE_VIOLATIONS.getKpiId());
 		removeForecastModel(KPICode.CODE_VIOLATIONS_KANBAN.getKpiId());
 		removeForecastModel(KPICode.TEST_EXECUTION_AND_PASS_PERCENTAGE.getKpiId());
-		removeForecastModel(KPICode.TEST_EXECUTION_TIME.getKpiId());
 		removeForecastModel(KPICode.TEST_EXECUTION_KANBAN.getKpiId());
 
 		// KPIs for Iteration Board.
