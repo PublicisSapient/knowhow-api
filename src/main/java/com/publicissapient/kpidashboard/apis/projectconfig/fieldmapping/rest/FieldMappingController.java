37037d0e8c735c3d1766ac2d4a29d8c0bd62603b
vladinu <vladinu@publicisgroupe.net>
2025-12-17 16:05:09 +0200

    Solve Sonar issues

--- src/main/java/com/publicissapient/kpidashboard/apis/projectconfig/fieldmapping/rest/FieldMappingController.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/projectconfig/fieldmapping/rest/FieldMappingController.java
@@ -32,7 +32,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.publicissapient.kpidashboard.apis.abac.ContextAwarePolicyEnforcement;
 import com.publicissapient.kpidashboard.apis.appsetting.service.ConfigHelperService;
@@ -223,7 +227,7 @@ public class FieldMappingController {
 			ProjectBasicConfig projectBasicConfig =
 					fieldMappingService.getBasicProjectConfigById(
 							projectToolConfig.getBasicProjectConfigId());
-			policy.checkPermission(projectBasicConfig, "UPDATE_PROJECT");
+			policy.checkPermission(projectBasicConfig, UPDATE_PROJECT);
 
 			// validating kpicode
 			KPICode kpi = KPICode.getKPI(kpiId);
