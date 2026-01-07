84127e1116f844f9e305b7aa844e33aa76d89c1c
kunkambl <kunal.kamble@publicissapient.com>
2026-01-07 14:41:40 +0530

    DTS-52000: fixed test cases

--- src/main/java/com/publicissapient/kpidashboard/apis/common/service/ToolsKPIService.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/common/service/ToolsKPIService.java
@@ -813,7 +813,9 @@ public abstract class ToolsKPIService<R, S> {
 		Set<String> selectedIds = getSelectedIds(kpiRequest);
 		calculateThresholdValue(selectedIds, kpiElement, kpiRequest.getLabel());
 		for (String selectedId : selectedIds) {
-			Node node = nodeWiseKPIValue.get(Pair.of(kpiRequest.getSelecedHierarchyLabel().toUpperCase(), selectedId));
+			Node node =
+					nodeWiseKPIValue.get(
+							Pair.of(kpiRequest.getSelecedHierarchyLabel().toUpperCase(), selectedId));
 			if (null != node) {
 				Object obj = node.getValue();
 				Map<String, List<DataCount>> valueMap =
@@ -892,7 +894,9 @@ public abstract class ToolsKPIService<R, S> {
 		calculateThresholdValue(selectedIds, kpiElement, kpiRequest.getLabel());
 
 		for (String selectedId : selectedIds) {
-			Node node = nodeWiseKPIValue.get(Pair.of(kpiRequest.getSelecedHierarchyLabel().toUpperCase(), selectedId));
+			Node node =
+					nodeWiseKPIValue.get(
+							Pair.of(kpiRequest.getSelecedHierarchyLabel().toUpperCase(), selectedId));
 			if (null != node) {
 				Object obj = node.getValue();
 				Map<String, List<DataCount>> valueMap =
@@ -974,7 +978,7 @@ public abstract class ToolsKPIService<R, S> {
 
 		if (Constant.SPRINT.equalsIgnoreCase(kpiRequest.getLabel())) {
 			populateSelectedIdInSprintSelection(kpiRequest, selectedIds);
-            kpiRequest.setSelecedHierarchyLabel(Constant.PROJECT.toUpperCase());
+			kpiRequest.setSelecedHierarchyLabel(Constant.PROJECT.toUpperCase());
 		} else if (MapUtils.isNotEmpty(addFilterCat)
 				&& null != addFilterCat.get(kpiRequest.getLabel().toUpperCase())) {
 			Map<String, List<String>> kpiRequestSelectedMap = new HashMap<>();
@@ -989,10 +993,10 @@ public abstract class ToolsKPIService<R, S> {
 			} else {
 				selectedIds.addAll(kpiRequestSelectedMap.get(Constant.PROJECT.toUpperCase()));
 			}
-            kpiRequest.setSelecedHierarchyLabel(Constant.PROJECT.toUpperCase());
+			kpiRequest.setSelecedHierarchyLabel(Constant.PROJECT.toUpperCase());
 		} else {
 			selectedIds = new HashSet<>(Arrays.asList(kpiRequest.getIds()));
-            kpiRequest.setSelecedHierarchyLabel(kpiRequest.getLabel());
+			kpiRequest.setSelecedHierarchyLabel(kpiRequest.getLabel());
 		}
 		return selectedIds;
 	}
