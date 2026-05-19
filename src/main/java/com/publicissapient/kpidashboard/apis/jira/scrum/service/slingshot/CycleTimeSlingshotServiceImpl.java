d1300895fb81cd3c8585e00b4ae6752ba217936c
kunkambl <kunal.kamble@publicissapient.com>
2026-05-19 09:08:12 +0530

    update userboard to hide slingshot by default

--- src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/slingshot/CycleTimeSlingshotServiceImpl.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/slingshot/CycleTimeSlingshotServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.stream.Collectors;
 
