c25f43f84f335d518fe1602ab2d8645764492bee
kunalaski <kunalkamble20k@gmail.com>
2026-06-18 12:47:03 +0530

    pr throughput

--- src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/slingshot/speed/ScmPRThroughputServiceImpl.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/slingshot/speed/ScmPRThroughputServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.Set;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.tuple.Pair;
