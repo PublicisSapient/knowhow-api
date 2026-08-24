d6f7c4ec5747ba8d03702075b043d7577a44d60b
Ananth Pal <ananth.pal@publicissapient.com>
2026-08-24 13:34:25 +0530

    Adding implementation for Refinement Cycle Time KPI

--- src/main/java/com/publicissapient/kpidashboard/apis/enums/FieldMappingEnum.java
+++ src/main/java/com/publicissapient/kpidashboard/apis/enums/FieldMappingEnum.java
@@ -931,6 +931,14 @@ public enum FieldMappingEnum {
 			KPISource.JENKINS.name(),
 			List.of("e2eTestBranchKPI220", "thresholdValueKPI220")),
 	KPI221("Change Failure Rate", KPISource.JENKINS.name(), List.of("thresholdValueKPI221")),
+	KPI222(
+			"Refinement Cycle Time",
+			KPISource.JIRA.name(),
+			List.of(
+					"jiraStoryIdentificationKPI222",
+					"jiraStatusToStartRefinementKPI222",
+					"jiraStatusForRefinementKPI222",
+					"thresholdValueKPI222")),
 	KPI311(
 			"Story Hygiene",
 			KPISource.JIRA.name(),
